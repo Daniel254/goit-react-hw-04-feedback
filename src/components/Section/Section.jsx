57c55fa971606868dd8935305409ca7f86142d8c
Daniel254 <Daniel.laskin@gmail.com>
2022-07-27 23:38:10 +0300

    Refactoring to FC done

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,19 +1,17 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 
-export default class Section extends Component {
-  static propTypes = {
-    title: PropTypes.string,
-    children: PropTypes.element.isRequired,
-  };
-
-  render() {
-    const { title, children } = this.props;
-    return (
-      <section>
-        {title && <h2>{title}</h2>}
-        {children}
-      </section>
-    );
-  }
+function Section({ title, children }) {
+  return (
+    <section>
+      {title && <h2>{title}</h2>}
+      {children}
+    </section>
+  );
 }
+
+Section.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.element.isRequired,
+};
+
+export default Section;
